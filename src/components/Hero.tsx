d70a9439fe3a913bf495a8a0347da946d060c8d0
designer789 <designer0510@outlook.com>
2025-04-24 11:18:55 +0800

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -63,6 +63,7 @@ export default function Hero() {
                 <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
               </button>
 
+              <a href="https://rankchain.gitbook.io/docs/" target="_blank" rel="noopener">
               <button className="bg-zinc-900/10 no-underline group cursor-pointer relative shadow-xl shadow-zinc-900/10 rounded-full p-px text-sm font-semibold leading-6 text-black inline-block">
                 <span className="absolute inset-0 overflow-hidden rounded-full">
                   <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(56,189,248,0.3)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
@@ -87,6 +88,7 @@ export default function Hero() {
                 </div>
                 <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-zinc-400/0 via-zinc-400/60 to-zinc-400/0 transition-opacity duration-500 group-hover:opacity-40" />
               </button>
+                </a>
             </div>
           </div>
           
